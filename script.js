36ead50f21d8f8aae57e72671de3f407ac5a417c
JeongTaekCho <92679073+JeongTaekCho@users.noreply.github.com>
2022-08-19 01:38:35 +0900

    add function

---
+++ script.js
@@ -0,0 +1,72 @@
+const todoList = document.querySelector("#todo_list");
+const todoInput = document.querySelector("#todo-input");
+
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = (storageData) => {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+    saveItemsFn();
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+
+  if (storageData && storageData.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+
+  saveItemsFn();
+};
+
+const keyCodeCheck = () => {
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createTodo();
+  }
+};
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll("li");
+  for (let x of liList) {
+    x.remove();
+  }
+  saveItemsFn();
+};
+
+const saveItemsFn = () => {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createTodo(savedTodoList[i]);
+  }
+}
